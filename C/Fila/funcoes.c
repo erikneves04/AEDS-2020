30cfc2f87bee7d35f55f4513436143243cd4bda3
erikneves04 <erikrrn04@gmail.com>
2020-09-02 16:04:38 -0300

    Implementação de Fila

---
+++ C/Fila/funcoes.c
@@ -0,0 +1,92 @@
+/*
+ * @file   funcoes.c
+ * @brief  Implementaçao das funçoes de manipulaçao de lista encadeada.
+ * @author <Erik Neves>
+ * @date   2020-08-31
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "const.h"
+
+void Inicializar_lista(Lista * lista){
+    lista->Numero_de_itens = 0;
+    lista->primeiro = NULL;
+    lista->ultimo = NULL;
+}
+Error Insere_dado(DataType Valor,Lista * lista){
+    /* INSERE NO FINAL DA LISTA */
+    Item_lista * Novo_dado = (Item_lista*)malloc(sizeof(Item_lista));
+
+    if(lista->Numero_de_itens == 0){
+        Novo_dado->proximo = lista->primeiro;
+        Novo_dado->anterior = NULL;
+        lista->primeiro = Novo_dado;
+        lista->ultimo = Novo_dado;
+    }else{
+        Novo_dado->proximo = lista->ultimo;
+        Novo_dado->anterior = NULL;
+        lista->ultimo->anterior = Novo_dado;
+        lista->ultimo = Novo_dado;
+    }
+
+    Novo_dado->Dado = Valor;
+    lista->Numero_de_itens++;
+
+    return Sucesso;
+}
+Error Remove_dado(Lista * lista){
+    DataType Aux = lista->ultimo->Dado;
+    Item_lista * segundo = lista->primeiro->anterior;
+
+    if(lista->Numero_de_itens == 0){
+        return Erro_lista_vazia;
+    }
+    free(lista->primeiro);
+    lista->Numero_de_itens--;
+    lista->primeiro = segundo;
+    return Aux;
+}
+Boolean Lista_vazia(Lista * lista){
+    if(lista->Numero_de_itens == 0 && lista->primeiro == NULL && lista->ultimo == NULL){
+        return true;
+    }else{
+        return false;
+    }
+}
+Error Imprimir_lista(Lista * lista){
+    int i,j;
+    char LinhaImpressao[(21 + sizeof(DataType))];
+    Item_lista * Dados_lista = lista->primeiro;
+
+    if(Lista_vazia(lista) != true){
+        printf("Imprimindo dados da lista: \n");
+        printf("Indi.       Info.\n");
+        for(i=0,j=1;i<(lista->Numero_de_itens);i++,j++){ 
+            (void)sprintf(LinhaImpressao," %%%s          %%%s\n",".2d",PrintfType);
+            (void)printf(LinhaImpressao,j,Dados_lista->Dado);
+            Dados_lista = Dados_lista->anterior; 
+        }
+        printf("\n");
+    }else{
+        return Erro_lista_vazia;
+    }
+    return Sucesso;
+}
+Error Limpar_lista(Lista * lista){
+    Item_lista * Dados_lista = lista->primeiro;
+    Item_lista * Proximo_aux = NULL;
+    int i; 
+
+    if(Lista_vazia(lista) == true){
+        return Erro_lista_vazia;
+    }
+    for(i=0;i<lista->Numero_de_itens;i++){
+        Proximo_aux = Dados_lista->anterior;
+        free(Dados_lista);
+        Dados_lista = Proximo_aux;
+    }
+    Inicializar_lista(lista);
+    return Sucesso;
+}
---
+++ C/Fila/main.c
@@ -0,0 +1,79 @@
+/*
+ * @file   main.c
+ * @brief  
+ * @author <Erik Neves>
+ * @date   2019-11-13
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "const.h"
+#include "funcoes.h"
+
+static DataType UserIteract(void){
+    DataType Interact_aux;
+    char Identifier[] = "%";
+
+    strcat(Identifier,PrintfType);
+
+    printf("\nDigite o dado(%s): ",PrintfType_descripition);
+    scanf(Identifier,&Interact_aux);
+
+    printf("\n");
+    return Interact_aux;
+}
+
+int main(void){
+
+    Lista * lista = (Lista*)malloc(sizeof(Lista));
+    int Escolha_do_usuario=Variavel_de_inicio;
+
+    Inicializar_lista(lista);
+    while(Escolha_do_usuario!=Encerrar_loop){
+
+        printf("+--------------------+\n");
+        printf("| Implementacao Fila |\n");
+        printf("+--------------------+\n");
+        printf("[0] Encerra o software.\n");
+        printf("[1] Inserir no final.\n");
+        printf("[2] Remover do inicio.\n");
+        printf("[3] Imprimir dados.\n");
+        printf("[4] Estado da lista.\n");
+        printf("[5] Limpar lista.\n");
+        printf("Escolha: ");
+        scanf("%d", &Escolha_do_usuario);
+        printf("\n");
+
+
+        switch(Escolha_do_usuario){
+            
+            case 0:
+                printf(" Encerrando. \n\n");
+            break;
+            case 1:
+                Insere_dado(UserIteract(),lista);
+            break;
+            case 2:
+                Remove_dado(lista);
+            break;
+            case 3:
+                Imprimir_lista(lista);
+            break;
+            case 4:
+                if(Lista_vazia(lista) == true){
+                    printf("\n Esta vazia: true\n\n");
+                }else{
+                    printf("\n Esta vazia: false\n\n");
+                }
+            break;
+            case 5:
+                Limpar_lista(lista);
+            break;
+            default:
+                printf(" Invalido. \n\n");
+            break;
+        }
+    }
+    return Sucesso;
+}
