034629e22e04127298b7e5461c0635a7ac976378
erikneves04 <erikrrn04@gmail.com>
2020-11-07 19:10:58 -0300

    Fila (C) - Versão 2.0

--- C/Fila/funcoes.h
+++ C/Fila/funcoes.h
@@ -1,17 +1,17 @@
 /**
 * @file   funcoes.h
-* @brief  Arquivo com as funçoes de manipulação de fila.
+* @brief  Arquivo com as assinaturas das funções de manipulação de lista encadeada.
 * @author <Erik Neves>
-* @date   2020-08-31
+* @date   2020-11-07
 */
 #pragma once
 
 #ifndef __funcoes_h__
 #define __funcoes_h__
 
-Error Inicializar_lista(Lista * lista);
+Error Inicializar_lista(Lista * lista,DataTypeCompare compare,PrintDataType print);
 Error Insere_dado(DataType Valor,Lista * lista);
-Error Remove_dado(Lista * lista);
+DataType Remove_dado(Lista * lista);
 Boolean Lista_vazia(Lista * lista);
 Error Imprimir_lista(Lista * lista);
 Error Limpar_lista(Lista * lista);
