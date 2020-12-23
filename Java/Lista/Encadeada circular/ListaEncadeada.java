68f9ba645a4973a2737944b80b617acac7d01071
erikneves04 <erikrrn04@gmail.com>
2020-12-23 10:53:50 -0300

    Update Listas Encadeadas - JAVA

--- Java/Lista/Encadeada circular/ListaEncadeada.java
+++ Java/Lista/Encadeada circular/ListaEncadeada.java
@@ -154,12 +154,45 @@ public class ListaEncadeada<DataType>{
         return DadosLista.GetDadosItem();
     } 
 
-    //apagar dps
-    public void sout(){
-        ItemLista<DataType> DadosLista = this.primeiro;
-        for(int i=0;i<this.NumeroDeItens * 2;i++){
-            System.out.println(DadosLista.GetDadosItem());
-            DadosLista = DadosLista.GetProximo();
+    /**
+     * List item responsible for encapsulating data DataType and allowing this reference of the posterior.
+     * @author Erik Neves
+     * @param <DataType> Type of data that will be stored and manipulated.
+    */
+    private class ItemLista<DataType> {  
+        private DataType dados;
+        private ItemLista<DataType> proximo;
+        
+        /**
+        * Method responsible for initialize the data for this object.
+        * @param Data The object to be stored.
+        * @param prox Item after the current item.
+        */
+        public void InicializarItem(DataType Data, ItemLista<DataType> prox){
+            this.dados = Data;
+            this.proximo = prox;
         }
+        
+        /**
+        * Method responsible for replace the currently saved object with another one.
+        * @param NewDados New object.
+        */
+        public void SetDadosItem(DataType NewDados){this.dados = NewDados;}
+        /**
+        * Method responsible for return an object stored in the list.
+        * @return The object with the data.
+        */
+        public DataType GetDadosItem(){return this.dados;}
+        
+        /**
+        * Method responsible for replace the reference of the next list item.
+        * @param prox Next item to be indexed.
+        */
+        public void SetProximo(ItemLista<DataType> prox){this.proximo = prox;}
+        /**
+        * Method responsible for return the next referenced item.
+        * @return Next item.
+        */
+        public ItemLista<DataType> GetProximo(){return this.proximo;} 
     }
 }
--- Java/Lista/Encadeada/ListaEncadeada.java
+++ Java/Lista/Encadeada/ListaEncadeada.java
@@ -140,5 +140,47 @@ public class ListaEncadeada<DataType>{
             if(DadosLista.GetProximo() != null)DadosLista = DadosLista.GetProximo();
         }
         return DadosLista.GetDadosItem();
+    }
+
+    /**
+     * List item responsible for encapsulating data DataType and allowing this reference of the posterior.
+     * @author Erik Neves
+     * @param <DataType> Type of data that will be stored and manipulated.
+     */
+    private class ItemLista<DataType> {  
+        private DataType dados;
+        private ItemLista<DataType> proximo;
+        
+        /**
+        * Method responsible for initialize the data for this object.
+        * @param Data The object to be stored.
+        * @param prox Item after the current item.
+        */
+        public void InicializarItem(DataType Data, ItemLista<DataType> prox){
+            this.dados = Data;
+            this.proximo = prox;
+        }
+        
+        /**
+        * Method responsible for replace the currently saved object with another one.
+        * @param NewDados New object.
+        */
+        public void SetDadosItem(DataType NewDados){this.dados = NewDados;}
+        /**
+        * Method responsible for return an object stored in the list.
+        * @return The object with the data.
+        */
+        public DataType GetDadosItem(){return this.dados;}
+        
+        /**
+        * Method responsible for replace the reference of the next list item.
+        * @param prox Next item to be indexed.
+        */
+        public void SetProximo(ItemLista<DataType> prox){this.proximo = prox;}
+        /**
+        * Method responsible for return the next referenced item.
+        * @return Next item.
+        */
+        public ItemLista<DataType> GetProximo(){return this.proximo;} 
     }  
 }
